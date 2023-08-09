a8f8feff5360cdbbf3c3d0dd53d23e39b01c9e06
venkatvallabhaneni <venkat.vallabhaneni@thoughtworks.com>
2023-08-09 16:52:53 +0530

    Draft Chat GPT API integration

---
+++ app/README.md
@@ -0,0 +1,16 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Steps:
+
+1. npm create vite@latest app -- --template react 
+2. cd app
+3. npm install
+4. npm install @chatscope/chat-ui-kit-react
---
+++ app/src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
+
+function App() {
+
+  const API_KEY = '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';
+
+  const [typing, setTyping] = useState(false);
+
+  const [messages, setMessages] = useState([{
+    message: 'Hello , I am ChatGPT',
+    sender: 'ChatGPT'
+  }])
+
+  const handleSend = async (message) => {
+
+    const newMessage = {
+      'message': message,
+      'sender': 'Venkat V',
+      'direction': 'outgoing'
+    }
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    setTyping(true);
+    await processChatMessagesWithChatGPTTurbo(newMessages);
+  }
+
+  async function processChatMessagesWithChatGPTTurbo(chatMessages) {
+
+    let apiMessage = chatMessages.map((messageObject) => {
+
+      let role = '';
+
+      if (messageObject.sender === 'ChatGPT') {
+        role = 'assistant';
+      } else {
+        role = 'user';
+
+      }
+      return { 'role': role, 'content': messageObject.message }
+
+    });
+
+    const defaultMessage = {
+      'role': 'system',
+      'content': 'Explain what is chatGPT'
+    }
+    const apiMessageRequestBody = {
+      'model': 'gpt-3.5-turbo',
+      'messages': [defaultMessage, ...apiMessage]
+
+    }
+
+    await fetch('https://api.openai.com/v1/chat/completions', {
+      'method': 'POST',
+      'headers': {
+        Authorization: 'Bearer ' + API_KEY,
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify(apiMessageRequestBody),
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data)
+      setMessages([...chatMessages,
+      {
+        message: data.choices[0].message.content,
+        sender: 'ChatGPT'
+      }
+      ])
+    })
+  }
+
+  return (
+
+    <div className='App'>
+
+      <div style={{ position: 'relative', height: '800px', width: '600px' }}>
+
+        <MainContainer>
+          <ChatContainer>
+            <MessageList typingIndicator={typing ? <TypingIndicator content='Chat Gpt is Typing' /> : null}>
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type Message Here' onSend={handleSend}></MessageInput>
+          </ChatContainer>
+        </MainContainer>
+      </div>
+
+
+    </div>
+
+  )
+}
+
+export default App
